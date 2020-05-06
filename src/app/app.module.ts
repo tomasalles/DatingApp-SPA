b2fe015fd4e2fa05b33a55ee02c212b8d25fb28f
Tomas Sales <tomas2_salles@hotmail.com>
2020-05-06 12:40:35 +0100

    add routes and guards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from "@angular/core"
 import { HttpClientModule } from "@angular/common/http"
 import { FormsModule } from "@angular/forms"
 import { BsDropdownModule } from "ngx-bootstrap/dropdown"
+import { RouterModule } from "@angular/router"
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 
 import { AppComponent } from "./app.component"
@@ -11,15 +12,28 @@ import { AuthService } from "./services/auth.service"
 import { HomeComponent } from "./home/home.component"
 import { RegisterComponent } from "./register/register.component"
 import { ErrorInterceptorProvider } from "./services/error.interceptor"
+import { MemberListComponent } from "./member-list/member-list.component"
+import { ListsComponent } from "./lists/lists.component"
+import { MessagesComponent } from "./messages/messages.component"
+import { appRoutes } from "./routes"
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
+    MemberListComponent,
+    ListsComponent,
+    MessagesComponent
+  ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     BsDropdownModule.forRoot(),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [AuthService, ErrorInterceptorProvider],
   bootstrap: [AppComponent]
