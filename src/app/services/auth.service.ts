0b56df0261856eb731d11aa9f7d0d24adccb8a61
Tomas Sales <tomas2_salles@hotmail.com>
2020-05-06 12:03:31 +0100

    add features

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,21 +1,25 @@
 import { Injectable } from "@angular/core"
 import { HttpClient } from "@angular/common/http"
 import { map } from "rxjs/operators"
+import { JwtHelperService } from "@auth0/angular-jwt"
 
 @Injectable({
   providedIn: "root"
 })
 export class AuthService {
   baseUrl = "http://localhost:5000/api/auth/"
+  jwtHelper = new JwtHelperService()
+  decodedToken: any
 
   constructor(private http: HttpClient) {}
 
   login(model) {
     return this.http.post(this.baseUrl + "login", model).pipe(
-      map((response: { token }) => {
+      map((response: { token: string }) => {
         const user = response
         if (user) {
           localStorage.setItem("token", user.token)
+          this.decodedToken = this.jwtHelper.decodeToken(user.token)
         }
       })
     )
@@ -23,5 +27,10 @@ export class AuthService {
 
   register(model) {
     return this.http.post(this.baseUrl + "register", model)
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem("token")
+    return !this.jwtHelper.isTokenExpired(token)
   }
 }
