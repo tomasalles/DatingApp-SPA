b22fd0f65617ac113cdef57c5894fe84e300fb85
Tomas Sales <tomas2_salles@hotmail.com>
2020-05-08 10:34:31 +0100

    Finish registration form

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from "@angular/core"
 import { HttpClient } from "@angular/common/http"
+import { BehaviorSubject } from "rxjs"
 import { map } from "rxjs/operators"
 import { JwtHelperService } from "@auth0/angular-jwt"
 import { environment } from "src/environments/environment"
+import { User } from "../_models/user"
 
 @Injectable({
   providedIn: "root"
@@ -11,23 +13,33 @@ export class AuthService {
   baseUrl = environment.apiUrl + "auth/"
   jwtHelper = new JwtHelperService()
   decodedToken: any
+  currentUser: User
+  photoUrl = new BehaviorSubject<string>("../../assets/user.png")
+  currentPhotoUrl = this.photoUrl.asObservable()
 
   constructor(private http: HttpClient) {}
 
+  changeMemberPhoto(photoUrl: string) {
+    this.photoUrl.next(photoUrl)
+  }
+
   login(model) {
     return this.http.post(this.baseUrl + "login", model).pipe(
-      map((response: { token: string }) => {
+      map((response: { token: string; user: User }) => {
         const user = response
         if (user) {
           localStorage.setItem("token", user.token)
+          localStorage.setItem("user", JSON.stringify(user.user))
           this.decodedToken = this.jwtHelper.decodeToken(user.token)
+          this.currentUser = user.user
+          this.changeMemberPhoto(this.currentUser.photoUrl)
         }
       })
     )
   }
 
-  register(model) {
-    return this.http.post(this.baseUrl + "register", model)
+  register(user: User) {
+    return this.http.post(this.baseUrl + "register", user)
   }
 
   loggedIn() {
--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -19,4 +19,19 @@ export class UserService {
   getUser(id: number): Observable<User> {
     return this.http.get<User>(this.baseUrl + "users/" + id)
   }
+
+  updateUser(id: number, user: User) {
+    return this.http.put(this.baseUrl + "users/" + id, user)
+  }
+
+  setMainPhoto(userId: number, id: number) {
+    return this.http.post(
+      this.baseUrl + "users/" + userId + "/photos/" + id + "/setMain",
+      {}
+    )
+  }
+
+  deletePhoto(userId: number, id: number) {
+    return this.http.delete(this.baseUrl + "users/" + userId + "/photos/" + id)
+  }
 }
